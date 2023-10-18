28cb6ac8cd46b59b0acd1819a592546160e70904
Muhammad Nouman Attique <108257688+kingpin147@users.noreply.github.com>
2023-10-18 15:04:55 +0500

    python crash course clases

---
+++ class1/class1.py
@@ -0,0 +1,15 @@
+print("hello world")
+print("/n")
+
+name:str ="Nouman Attique"
+# name = 700
+print(name) # print
+print(type(name)) #type
+print(id(name)) #  physical address
+print(dir(name))  #methods directory
+
+name1:str = 700
+print(name1) 
+
+# name2:list[str] = [a, b, c, d, e, f, g]
+# name3:tuple[str,int,float] = (b, 2,1.2)
